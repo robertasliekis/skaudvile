6f7688a2a8c1836455504b7a306814b51a30ebc9
Robertas Liekis <robertasliekis@gmail.com>
2020-12-17 01:45:06 +0200

    initial commit

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ModelViewerThree from "./ModelViewerThree";
+// import ModelViewerThree from "./ModelViewerThree";
 const mapImagesArray = [8, 7, 6, 5, 4, 3, 2, 1];
 const activeIconsArray = [
   [
